8e1bf6d5749a710cc3f5e0ff6c5decf8895d7093
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-09-15 20:27:02 +0530

    Fix bug where contacts had remove button

--- src/components/form/SectionItemForm.js
+++ src/components/form/SectionItemForm.js
@@ -2,24 +2,38 @@ import react from 'react';
 
 class SidebarItemForm extends react.Component {
   render() {
-    return (
-      <div className="form-wrapper">
-        <label htmlFor="">{this.props.title}</label>
-        <input
-          id={this.props.id}
-          type="text"
-          value={this.props.value}
-          onChange={this.props.onChange}
-        />
-        <button
-          className="remove-item-button"
-          id={this.props.id}
-          onClick={this.props.deleteAwardEntry}
-        >
-          Remove item
-        </button>
-      </div>
-    );
+    if (this.props.title === 'Achievement') {
+      return (
+        <div className="form-wrapper">
+          <label htmlFor="">{this.props.title}</label>
+          <input
+            id={this.props.id}
+            type="text"
+            value={this.props.value}
+            onChange={this.props.onChange}
+          />
+          <button
+            className="remove-item-button"
+            id={this.props.id}
+            onClick={this.props.deleteAwardEntry}
+          >
+            Remove item
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="form-wrapper">
+          <label htmlFor="">{this.props.title}</label>
+          <input
+            id={this.props.id}
+            type="text"
+            value={this.props.value}
+            onChange={this.props.onChange}
+          />
+        </div>
+      );
+    }
   }
 }
 
